c47979e083d1b2f134c8eae57ead938d1aaa49f3
Ross McIlwaine <rdm0423@gmail.com>
2015-01-30 16:00:07 -0700

    Step 2 complete, started step 3

--- NumberProblems-Objc/NumberProblems-Objc/AppDelegate.m
+++ NumberProblems-Objc/NumberProblems-Objc/AppDelegate.m
@@ -8,6 +8,11 @@
 
 #import "AppDelegate.h"
 
+//declare a global variable
+
+int globalInt = 8;
+
+
 @interface AppDelegate ()
 
 @end
@@ -15,12 +20,44 @@
 @implementation AppDelegate
             
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
 
     // Insert code here...
+    
+
+    [self iterateCount:4];
+   
+    
+    
+    
+    
+
     
     
     return YES;
 }
+
+
+-(void)iterateCount:(int)x
+{
+    if (x == 0) {
+        NSLog(@"Iterate %d", x);
+    }
+    else
+    {
+        NSLog(@"Iterate %d", x);
+        int oneLess = x - 1;
+        [self iterateCount:oneLess];
+    }
+}
+
+-(void)multiplied:(int)y
+{
+    int multiplied = y * globalInt;
+    NSLog(@"%d", multiplied);
+
+}
+
 
 @end
